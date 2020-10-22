c5806164488461aba4eb3b36d08e54b719496e73
kail <kail@tvws.com.tw>
2020-10-23 07:11:00 +0800

    using props.children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const person = (props) => {
   return (
-    <p>
-      I'm a {props.name} and age is {props.age}
-    </p>
+    <div>
+      <p>
+        I'm a {props.name} and age is {props.age}
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
