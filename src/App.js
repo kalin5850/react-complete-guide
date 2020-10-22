9e6d8c9e9abb8b7fc2de59d07d1ec65ab91262d2
kail <kail@tvws.com.tw>
2020-10-22 22:40:51 +0800

    funtional components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi, I'm a React App</h1> 
+        <h1>Hi, I'm a React App</h1>
+        <br />
+        <h1>test</h1>
+        <Person />
       </div>
     );
     /**
      *  same as upper return statement
      */
-    // return React.createElement('div', {className: 'App'}, 
+    // return React.createElement('div', {className: 'App'},
     //        React.createElement('h1', null, 'Does this work now?'));
   }
 }
